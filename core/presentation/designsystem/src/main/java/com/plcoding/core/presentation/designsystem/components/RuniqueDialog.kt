960d4079f97227b9059feff1af2433e1b11f0d14
sahansarki <sahan.sarki@nesine.com>
2024-07-27 00:24:01 +0300

    Set up permission handling

---
+++ core/presentation/designsystem/src/main/java/com/plcoding/core/presentation/designsystem/components/RuniqueDialog.kt
@@ -0,0 +1,89 @@
+package com.plcoding.core.presentation.designsystem.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.plcoding.core.presentation.designsystem.RuniqueTheme
+
+@Composable
+fun RuniqueDialog(
+    title: String,
+    onDismiss: () -> Unit,
+    description: String,
+    primaryButton: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    secondaryButton: @Composable () -> Unit = {}
+) {
+    Dialog(onDismissRequest = onDismiss) {
+        Column(
+            modifier = modifier
+                .clip(RoundedCornerShape(15.dp))
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(15.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = title,
+                fontWeight = FontWeight.SemiBold,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Text(
+                text = description,
+                fontSize = 12.sp,
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                secondaryButton()
+                primaryButton()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun RuniqueDialogPrev() {
+    RuniqueTheme {
+        RuniqueDialog(
+            title = "Sample Dialog Title",
+            onDismiss = { /* Do something when the dialog is dismissed */ },
+            description = "This is a sample description for the dialog. It provides more details about the purpose of the dialog.",
+            primaryButton = {
+                Button(onClick = { /* Do something when the primary button is clicked */ }) {
+                    Text("Confirm")
+                }
+            },
+            secondaryButton = {
+                Button(onClick = { /* Do something when the secondary button is clicked */ }) {
+                    Text("Cancel")
+                }
+            }
+        )
+    }
+}
